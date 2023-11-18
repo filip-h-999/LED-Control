16bb19dae73314d4996f262d8a7930ed7ef34f91
filip-h-999 <filip.hrenovcik@gmail.com>
2023-11-18 16:51:43 +0100

    Refactor color picker and add switch functionality

--- static/index.js
+++ static/index.js
@@ -1,39 +1,58 @@
-const socket = new WebSocket('ws://' + location.host + '/echo');
+const socket = new WebSocket("ws://" + location.host + "/echo");
 
-window.onload = function () {
-	var colorPicker = new iro.ColorPicker(".colorPicker", {
-		width: 280,
-		color: "rgb(255, 120, 0)",
-		borderWidth: 0.5,
-		borderColor: "#171F30",
-	});
+document.addEventListener("DOMContentLoaded", function () {
+	let hex;
 
-	var values = document.getElementById("values");
-	var hexInput = document.getElementById("hexInput");
+	function switchOnOff() {
+		if (!this.checked) {
+			socket.send("#000000");
+		} else {
+			socket.send(hex);
+		}
+	}
 
-	socket.addEventListener('open', function (event) {
-        // console.log('WebSocket connection opened.');
+	window.onload = function () {
+		// switchOnOff();
+		var colorPicker = new iro.ColorPicker(".colorPicker", {
+			width: 280,
+			color: "rgb(255, 95, 0)",
+			borderWidth: 0.5,
+			borderColor: "#171F30",
+		});
 
-        colorPicker.on(["color:init", "color:change"], function (color) {
-            values.innerHTML = [
-                "hex: " + color.hexString,
-                "rgb: " + color.rgbString,
-                "brightness: " + color.value + "%",
-            ].join("<br>");
+		var values = document.getElementById("values");
+		var hexInput = document.getElementById("hexInput");
 
-            hexInput.value = color.hexString;
+		socket.addEventListener("open", function (event) {
+			// Attach the validate function to the change event of the checkbox
+			const switchElement = document.getElementById("switch");
+			if (switchElement) {
+				switchElement.addEventListener("change", switchOnOff);
+			}
+			// console.log('WebSocket connection opened.');
 
-            var hex = color.hexString;
-            socket.send(hex);
-        });
-    });
-	
-	hexInput.addEventListener("change", function () {
-		colorPicker.color.hexString = this.value;
-	});
+			colorPicker.on(["color:init", "color:change"], function (color) {
+				values.innerHTML = [
+					"hex: " + color.hexString,
+					"rgb: " + color.rgbString,
+					"brightness: " + color.value + "%",
+				].join("<br>");
 
-	// socket.addEventListener('message', function (event) {
-	// 	console.log('Color: ', event.data);
-	// });
+				hexInput.value = color.hexString;
 
-}
+				hex = color.hexString;
+				if (switchElement.checked) {
+					socket.send(hex);
+				}
+			});
+		});
+
+		hexInput.addEventListener("change", function () {
+			colorPicker.color.hexString = this.value;
+		});
+
+		// socket.addEventListener('message', function (event) {
+		// 	console.log('Color: ', event.data);
+		// });
+	};
+});
