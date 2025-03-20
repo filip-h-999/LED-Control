30bcb8d0ad267bb53ad1c6e29c66b07c212523f7
filip-h-999 <filip.hrenovcik@gmail.com>
2025-03-20 18:17:41 +0100

    Refactor sunset.py to replace home detection logic with a request to check status

--- sunset.py
+++ sunset.py
@@ -1,6 +1,7 @@
 import asyncio
 import websockets
-import dd.py 
+# import dd.py 
+import requests
 
 async def start_on_sunset(uri, message):
     async with websockets.connect(uri) as websocket:
@@ -9,7 +10,12 @@ async def start_on_sunset(uri, message):
 websocket_uri = 'ws://raspberrypizero.local:8006/led'
 message_to_send = '#ff0800'
 
-dd.whoIsHome()
+# dd.whoIsHome()
 
-if dd.whoIsHome()[0]:
+# if dd.whoIsHome()[0]:
+#     asyncio.run(start_on_sunset(websocket_uri, message_to_send))
+
+# new version 2.0
+t = requests.get('raspberrypi.local:8009/status')
+if 'AtHome' in t.text:
     asyncio.run(start_on_sunset(websocket_uri, message_to_send))
