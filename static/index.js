a220b6c7fd7be0dc3ad5d652de99ab00a016e20b
filip-h-999 <filip.hrenovcik@gmail.com>
2025-09-19 13:17:29 +0200

    Update switch handling logic

--- static/index.js
+++ static/index.js
@@ -1,70 +1,71 @@
-const socket = new WebSocket("ws://" + location.host + "/led");
+const socket = new WebSocket("ws://" + location.host + "/echo");
 
 document.addEventListener("DOMContentLoaded", function () {
-	let hex;
-	let onOff = false;
-	let savedColor
+  let hex;
+  let isLedOn = false;
+  let savedColor;
 
-	const switchState = localStorage.getItem('switchState');
-    onOff = switchState;
+  const switchState = localStorage.getItem("switchState");
+  isLedOn = switchState === "true";
 
-    const color = localStorage.getItem('color');
-    savedColor = color;
+  const color = localStorage.getItem("color");
+  savedColor = color || "#ff1100"; // Default to red if no saved color
 
-	function switchOnOff() {
-        if (!this.checked) {
-            socket.send("#000000");
-            localStorage.setItem('switchState', false);
-        } else {
-            socket.send(savedColor);
-        }
+  function handleSwitchToggle() {
+    if (!this.checked) {
+      socket.send("#000000");
+      localStorage.setItem("switchState", "false");
+    } else {
+      socket.send(savedColor);
+      localStorage.setItem("switchState", "true");
     }
+  }
 
-	window.onload = function () {
-		// switchOnOff();
-		var colorPicker = new iro.ColorPicker(".colorPicker", {
-			width: 280,
-			color: savedColor,
-			borderWidth: 0.5,
-			borderColor: "#171F30",
-		});
+  window.onload = function () {
+    // switchOnOff();
+    var colorPicker = new iro.ColorPicker(".colorPicker", {
+      width: 280,
+      color: savedColor,
+      borderWidth: 0.5,
+      borderColor: "#171F30",
+    });
 
-		var values = document.getElementById("values");
-		var hexInput = document.getElementById("hexInput");
+    var values = document.getElementById("values");
+    var hexInput = document.getElementById("hexInput");
 
-		socket.addEventListener("open", function (event) {
-			// Attach the validate function to the change event of the checkbox
-			const switchElement = document.getElementById("switch");
-			switchElement.checked = onOff == 'true';
-			if (switchElement) {
-				switchElement.addEventListener("change", switchOnOff);
-			}
-			// console.log('WebSocket connection opened.');
+    socket.addEventListener("open", function (event) {
+      // Attach the validate function to the change event of the checkbox
+      const switchElement = document.getElementById("switch");
+      switchElement.checked = isLedOn;
+      if (switchElement) {
+        switchElement.addEventListener("change", handleSwitchToggle);
+      }
+      // console.log('WebSocket connection opened.');
 
-			colorPicker.on(["color:init", "color:change"], function (color) {
-				values.innerHTML = [
-					"hex: " + color.hexString,
-					"rgb: " + color.rgbString,
-					"brightness: " + color.value + "%",
-				].join("<br>");
+      colorPicker.on(["color:init", "color:change"], function (color) {
+        values.innerHTML = [
+          "hex: " + color.hexString,
+          "rgb: " + color.rgbString,
+          "brightness: " + color.value + "%",
+        ].join("<br>");
 
-				hexInput.value = color.hexString;
+        hexInput.value = color.hexString;
 
-				hex = color.hexString;
-				if (switchElement.checked) {
-                    localStorage.setItem('switchState', true);
-                    localStorage.setItem('color', hex);
-                    socket.send(hex);
-                }
-			});
-		});
+        hex = color.hexString;
+        savedColor = hex; // Update savedColor when color changes
+        localStorage.setItem("color", hex); // Always save color changes
+        if (switchElement.checked) {
+          socket.send(hex);
+        }
+      });
+    });
 
-		hexInput.addEventListener("change", function () {
-			colorPicker.color.hexString = this.value;
-		});
+    hexInput.addEventListener("change", function () {
+      colorPicker.color.hexString = this.value;
+    });
 
-		// socket.addEventListener('message', function (event) {
-		// 	console.log('Color: ', event.data);
-		// });
-	};
+    // socket.addEventListener('message', function (event) {
+    // 	console.log('Color: ', event.data);
+    // });
+  };
 });
